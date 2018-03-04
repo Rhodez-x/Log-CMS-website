e0f0df807784efd86863ea7eae052cb54aa3e341
Rhodez-x <jorn@guld-berg.dk>
2018-03-04 10:47:21 +0100

    Err

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 */
 
 $loginsidelevel = 0; // sidens login level 0 for ikke at skulle være logget ind og 1 for at skulle være logget ind. 
-$web_page_name = "Index"; // Sidens navn, dette navn afgører hvilken fane i menuen der er aktiv. (Skal være identisk med det i Mysql)
+$web_page_name = "ErrorIndex"; // Sidens navn, dette navn afgører hvilken fane i menuen der er aktiv. (Skal være identisk med det i Mysql)
 $sidenssti = "content/index"; // Dette er stien til hvor filerne ligger
 $overmodule = "/overmodule.php"; // Dette er overmoduleet, det jeg kalder for behandlingsfiler
 $content = "/content.php"; // Her er filnavnet på contentet af den pågældene side. 
