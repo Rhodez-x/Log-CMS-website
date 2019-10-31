92207d36a90b51bdf8aa614b43adc83980e5dbf4
Jørn Guldberg <jorn@guld-berg.dk>
2019-10-31 11:51:15 +0100

    fix #7 - if index page is set to deactivated then it display page under constrcution

--- content/index/content.php
+++ content/index/content.php
@@ -4,20 +4,31 @@
                 <?php
                     define("wow", "".MAIN_DB_PREFIX."");
                     $conn = get_db_connection(MAIN_DB_HOST, MAIN_DB_DATABASE_NAME, MAIN_DB_USER, MAIN_DB_PASS);
-                    $stmt = $conn->prepare("SELECT ".MAIN_DB_PREFIX."navi_name.name, ".MAIN_DB_PREFIX."text.text
-                                            FROM ".MAIN_DB_PREFIX."navi_name 
-                                            INNER JOIN ".MAIN_DB_PREFIX."text ON 
-                                            ".MAIN_DB_PREFIX."navi_name.parent_id=".MAIN_DB_PREFIX."text.parent_id
-                                            WHERE ".MAIN_DB_PREFIX."navi_name.name = 'Forside' 
-                                            AND ".MAIN_DB_PREFIX."navi_name.language = ?");
+                    $stmt = $conn->prepare("SELECT ".MAIN_DB_PREFIX."navi_name.name, ".MAIN_DB_PREFIX."text.text, ".MAIN_DB_PREFIX."navi.permission
+                            FROM ".MAIN_DB_PREFIX."navi_name 
+                            INNER JOIN ".MAIN_DB_PREFIX."text ON ".MAIN_DB_PREFIX."navi_name.parent_id=".MAIN_DB_PREFIX."text.parent_id
+                            INNER JOIN ".MAIN_DB_PREFIX."navi ON ".MAIN_DB_PREFIX."navi.id=".MAIN_DB_PREFIX."navi_name.parent_id
+                            WHERE ".MAIN_DB_PREFIX."navi.link = 'index' AND ".MAIN_DB_PREFIX."navi_name.language = ? AND link != place;");
                     $stmt->execute(array($_SESSION['session_language']));
                             // set the resulting array to associative
-                    if ($stmt->rowCount() == 1) {
-                        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-                        foreach($stmt->fetchAll() as $row) {
-                            echo $row['text'];
+                    if ($stmt->rowCount() == 1) 
+                    {
+                        foreach($stmt->fetchAll() as $row) 
+                        {
+                            if (check_permission($row['permission']) )
+                            {
+                                echo $row['text'];
+                            }
+                            else 
+                            {
+                                echo "<h2>Page is under construction</h2>";
+                            }
                         }
-                    } 
+                    }
+                    else 
+                    {
+                        echo "<h2>Error 404</h2> Siden er ikke fundet";
+                    }
                 ?>
             </div>
     </div>
